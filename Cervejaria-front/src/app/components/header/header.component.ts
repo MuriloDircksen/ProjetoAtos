569099354980f9fa112b22923bd2ed4136d4fdaa
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-05-31 10:18:03 -0300

    adição header e menu lateral

--- Cervejaria-front/src/app/components/header/header.component.ts
+++ Cervejaria-front/src/app/components/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  sair(){
 
+    localStorage.setItem('segurança', 'false');
+  }
 }
