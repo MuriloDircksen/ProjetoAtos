5ffb04c9cb0cc30cb8cbaff48a57e04543d5df50
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-06-16 11:18:35 -0300

    implementado atualização e deleção de receitas

--- Cervejaria-front/src/app/pages/receita/modifica-receita/modifica-receita.component.ts
+++ Cervejaria-front/src/app/pages/receita/modifica-receita/modifica-receita.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IIngredientes } from 'src/app/models/ingredientes';
+import { IReceitaIngrediente } from 'src/app/models/receita-ingrediente';
 import { IReceitas } from 'src/app/models/receitas';
 import { IngredienteServiceService } from 'src/app/service/ingrediente/ingrediente-service.service';
 import { ReceitasServiceService } from 'src/app/service/receita/receita-service.service';
@@ -19,8 +20,10 @@ export class ModificaReceitaComponent implements OnInit{
   receita!: IReceitas;
   listaReceitas!: IReceitas[];
   listaIngredientes! : IIngredientes[];
-  quantidadeIngredientes : any[] = [];
+  listaReceitaIngrediente : IReceitaIngrediente[] = [];
+  quantidadeIngredientes : number = 0;
   orcamento : number = 0;
+  quantidadeReceitas : number = 0;
 
 
 
@@ -28,13 +31,13 @@ export class ModificaReceitaComponent implements OnInit{
     private formBuilder: FormBuilder, private router: Router,
     private ingredienteService : IngredienteServiceService){
       this.buscaIngredientes();
+      this.retornaQuantidadeIngredientes();
     }
 
   ngOnInit(): void {
     this.receitaId = this.activatedRoute.snapshot.paramMap.get('id')
     this.buscaReceita();
     this.formReceita;
-
 
   }
 
@@ -46,12 +49,17 @@ export class ModificaReceitaComponent implements OnInit{
   buscaReceita(){
     if(this.receitaId == "criar"){
       this.verificaTemId();
+      this.receitaService.getReceitas().subscribe((data)=>{
+        this.quantidadeReceitas= data[data.length-1].id;
+      })
       return;
    }
    this.receitaService.getReceita(parseFloat(this.receitaId)).subscribe((data)=>{
     this.receita = data;
+    this.orcamento = data.orcamento;
     this.verificaTemId();
    })
+
   }
 
   verificaTemId(){
@@ -63,13 +71,19 @@ export class ModificaReceitaComponent implements OnInit{
     }
     this.titulo = "Editar Receita"
     this.criaFormEdicao();
-    this.retornaQuantidadeIngredientes();
-
    }
 
     retornaQuantidadeIngredientes(){
-     this.receitaService.getReceitaIngredientes().subscribe((data)=>{
-       this.quantidadeIngredientes = data;
+      if(this.receitaId == "criar"){
+        return;
+      }
+      this.receitaService.getReceitaIngredientes().subscribe((data)=>{
+        this.listaReceitaIngrediente = data;
+        data.forEach((elemento)=>{
+        if(elemento.receitaId == this.receitaId){
+          this.quantidadeIngredientes +=1;
+        }
+       })
      })
     }
 
@@ -90,15 +104,18 @@ export class ModificaReceitaComponent implements OnInit{
       responsavelReceita: new FormControl(this.receita.responsavelReceita, Validators.required),
       estilo: new FormControl(this.receita.estilo, Validators.required),
       volumeReceita: new FormControl(this.receita.volumeReceita, Validators.required),
-      quantidadeIngredientes: new FormControl( 0 || this.quantidadeIngredientes.length, Validators.required),
+      quantidadeIngredientes: new FormControl(this.quantidadeIngredientes, Validators.required),
       ingredientes: this.formBuilder.array([])
     });
+    this.carregarIngredienteFormulario();
   }
 
   get quantidades(){
     return this.formReceita.get('quantidadeIngredientes')?.value;
   }
-
+  get volumeReceita(){
+    return this.formReceita.get('volumeReceita')?.value;
+  }
   get numeroIngredientes():FormArray {
     return this.formReceita.get('ingredientes') as FormArray;
   }
@@ -133,15 +150,14 @@ export class ModificaReceitaComponent implements OnInit{
         );
       }
     }
+    this.calculaOrcamento();
   }
 
     calculaOrcamento(){
-      if(this.receitaId == "criar"){
+
         this.orcamento = 0;
         this.numeroIngredientes.controls.forEach((control)=>{
           const obj = control.value;
-
-
           this.listaIngredientes.map((elemento)=>{
             if(elemento.id == obj.selectedIngredient){
               this.orcamento += obj.quantidade*elemento.valor_unidade;
@@ -149,84 +165,164 @@ export class ModificaReceitaComponent implements OnInit{
           })
         })
         return;
-     }
+
     }
 
-    modificaReceita(){}
-  // async modificaReceita(){
-  //   if(this.receitaId === "criar"){
-  //     const Receita: any= {
-  //       nomeReceita: this.nome,
-  //       responsavelReceita: this.responsavelReceita,
-  //       estilo: this.estacao,
-  //       ultima_atualizacao: new Date(),
-  //       orcamento: this.calculaOrcamento(),
-  //       lancamento: this.lancamento,
-  //       volumeReceita:
-  //     }
-  //     "nomeReceita": "Pilsen",
-  //     "responsavelReceita": "Murilo",
-  //     "estilo": "American Lager",
-  //     "ultima_atualizacao": "2023/04/20",
-  //     "orcamento": 9500,
-  //     "volumeReceita": 2000
+    criaIngrediente(): FormGroup {
+      return this.formBuilder.group({
+        selectedIngredient: '',
+        quantidade: ''
+      });
+    }
+    carregarIngredienteFormulario(){
+
+      this.listaReceitaIngrediente.forEach((elemento)=>{
+        this.listaIngredientes.forEach((ingrediente)=>{
+          if(elemento.ingredienteId == ingrediente.id){
+            this.adicionaIngrediente();
+            const control = this.numeroIngredientes;
+            const index = control.length-1;
+
+            control.at(index).patchValue({
+              selectedIngredient: ingrediente.id,
+              quantidade: elemento.quantidadeIngrediente
+            });
+          }
+        })
+      })
+    }
+
+    adicionaIngrediente() {
+      const control = this.numeroIngredientes;
+      control.push(this.criaIngrediente());
+    }
+
+  async modificaReceita(){
+    if(this.receitaId === "criar"){
+      const Receita: any= {
+        nomeReceita: this.nome,
+        responsavelReceita: this.responsavelReceita,
+        estilo: this.estilo,
+        ultima_atualizacao: new Date(),
+        orcamento: this.orcamento,
+        volumeReceita: this.volumeReceita
+      }
+
+      await this.receitaService.salvarReceita(Receita).toPromise();
+
+      this.modificaIngredienteReceita();
+      this.retornaPaginaReceita();
+      return;
+    }
+
+     const Receita: any= {
+        id: this.receita.id,
+        nomeReceita: this.nome,
+        responsavelReceita: this.responsavelReceita,
+        estilo: this.estilo,
+        ultima_atualizacao: new Date(),
+        orcamento: this.orcamento,
+        volumeReceita: this.volumeReceita
+     }
+
+    this.receitaService.atualizarReceita(Receita).subscribe();
+    this.alteraIngredientesReceita()
+    this.retornaPaginaReceita();
+
+  }
+
+     modificaIngredienteReceita(){
+      this.numeroIngredientes.controls.forEach((control)=>{
+        const ingredienteReceita : any = {
+          receitaId: this.quantidadeReceitas+1,
+          ingredienteId: control.value.selectedIngredient,
+          quantidadeIngrediente: control.value.quantidade
+        }
+       this.receitaService.salvarReceitaIngredientes(ingredienteReceita).toPromise();
+      })
+    }
+
+    alteraIngredientesReceita(){
+      let listaControleIngredientes: any = [];
+       this.numeroIngredientes.controls.forEach((control)=>{
+        const ingrediente: any = {
+            receitaId: parseInt(this.receitaId),
+           ingredienteId: parseInt(control.value.selectedIngredient),
+           quantidadeIngrediente: parseInt(control.value.quantidade)
+         }
+        listaControleIngredientes.push(ingrediente);
+        })
+
+        const ingredienteAdicionados = listaControleIngredientes.filter((ingredienteTeste : any) => !this.listaReceitaIngrediente.some(prevIng => prevIng.ingredienteId === ingredienteTeste.ingredienteId));
+        console.log("adicionar");
+
+        console.log(ingredienteAdicionados);
+        const removerIngredientes = this.listaReceitaIngrediente.filter(prevIng => !listaControleIngredientes.some((ingredienteTeste: any) => ingredienteTeste.ingredienteId === prevIng.ingredienteId));
+        console.log("remover");
+        console.log(removerIngredientes);
+        const alterarIngredientes = listaControleIngredientes.filter((ingredient:any) => {
+          const ingredientesAnterior = this.listaReceitaIngrediente.find(prevIng => prevIng.ingredienteId === ingredient.ingredienteId);
+          return ingredientesAnterior && (ingredientesAnterior.ingredienteId !== ingredient.ingredienteId || ingredientesAnterior.quantidadeIngrediente !== ingredient.quantidadeIngrediente);
+        });
+        console.log("mudar");
+        console.log(alterarIngredientes);
+
+        this.adicionaNovoIngredienteReceita(ingredienteAdicionados);
+        this.alteraNovoIngredienteReceita(alterarIngredientes);
+        this.removeNovoIngredienteReceita(removerIngredientes);
+
+    }
+    async adicionaNovoIngredienteReceita(novaLista: any){
+      novaLista.forEach((elemento: IReceitaIngrediente) => {
+        console.log(elemento);
+        this.receitaService.salvarReceitaIngredientes(elemento).toPromise();
+      });
+    }
+   async alteraNovoIngredienteReceita(novaLista: any){
+      novaLista.forEach((elemento: any) => {
+        this.listaReceitaIngrediente.forEach((data)=>{
+          if(elemento.ingredienteId == data.ingredienteId){
+            const ingredienteReceita : any = {
+              id: data.id,
+              receitaId:elemento.receitaId,
+              ingredienteId: elemento.ingredienteId,
+              quantidadeIngrediente: elemento.quantidadeIngrediente
+            }
+            console.log(ingredienteReceita);
+
+            this.receitaService.atualizarReceitaIngrediente(ingredienteReceita).toPromise();
+          }
+        })
+      });
+    }
+
+  async removeNovoIngredienteReceita(novaLista: any){
+      novaLista.forEach((elemento : any)=>{
+        console.log(elemento);
+        this.receitaService.excluirReceitaIngrediente(elemento.id).toPromise();
+      })
+    }
 
 
-  //     await this.ReceitaService.criarReceita(Receita).toPromise();
-  //     this.retornaPaginaReceita();
-  //     return;
-  //   }
-  //   const Receita: any= {
-  //     id: this.Receita.id,
-  //     nomeReceita: this.nomeReceita,
-  //     responsavelReceita: this.responsavelReceita,
-  //     estacao: this.estacao,
-  //     marca: this.marca,
-  //     orcamento: parseFloat(this.orcamento),
-  //     lancamento: this.lancamento,
 
-  //   }
+   excluiReceita(){
 
-  //   this.ReceitaService.atualizarReceita(Receita).subscribe();
-  //   this.retornaPaginaReceita();
+    this.receitaService.excluirReceita(this.receitaId).subscribe(); //está fazendo cascade all verificar depois regras com back end
+   // this.exlcuiIngredientes();
+    this.router.navigate(['/Receita']);
 
-  // }
-  // retornaModelos(){
-
-  //   this.modeloService.getModelos().subscribe((data) =>{
-  //     this.listaModelos=data;
-
+    // this.retornaPaginaReceita();
+   }
+  //  async exlcuiIngredientes(){ //está fazendo cascade all
+  //   this.listaReceitaIngrediente.forEach((data)=>{
+  //     this.receitaService.excluirReceitaIngrediente(data.id).toPromise();
   //   })
+  //  }
 
-  // }
+  retornaPaginaReceita(){
 
-  // retornaTotalModelos(id: number){
-  //   let numeroModelos = 0;
+      this.router.navigate(['/receitas']);
 
-  //   this.listaModelos.map((date) =>{
-
-  //     if(date.ReceitaId == id){
-  //       numeroModelos+=1;
-  //     }
-  //  })
-  //  return numeroModelos;
-  // }
-
-  // excluiReceita(){
-
-  //    if(this.retornaTotalModelos(this.ReceitaId) === 0){
-  //     this.ReceitaService.excluirReceita(this.ReceitaId).subscribe();
-  //     this.router.navigate(['/Receita']);
-  //     return;
-  //   }
-  //   alert("Impossível excluir coleções com modelos associados!")
-  //   this.retornaPaginaReceita();
-  // }
-
-  // retornaPaginaReceita(){
-
-  //     this.router.navigate(['/Receita']);
-
-  // }
+  }
 
 }
--- Cervejaria-front/src/app/service/receita/receita-service.service.ts
+++ Cervejaria-front/src/app/service/receita/receita-service.service.ts
@@ -10,7 +10,7 @@ import { IReceitas } from 'src/app/models/receitas';
 export class ReceitasServiceService {
 
   private url:string = "http://localhost:3000/receitas";
-  private url2:string ="receita_ingrediente"
+  private url2:string ="http://localhost:3000/receita_ingrediente"
 
   constructor(private http: HttpClient) { }
 
@@ -23,14 +23,22 @@ export class ReceitasServiceService {
   getReceita(id: number): Observable<IReceitas> {
     return this.http.get<IReceitas>(`${this.url}/${id}`)
   }
-
   salvarReceita(receita: IReceitas): Observable<IReceitas>{
     return this.http.post<IReceitas>(this.url, receita);
+  }
+  salvarReceitaIngredientes(receitaIngrediente : IReceitaIngrediente) :Observable<IReceitaIngrediente>{
+    return this.http.post<IReceitaIngrediente>(this.url2, receitaIngrediente);
   }
   atualizarReceita(receita: IReceitas): Observable<IReceitas> {
     return this.http.put<IReceitas>(`${this.url}/${receita.id}`, receita);
   }
+  atualizarReceitaIngrediente(receitaIngrediente : IReceitaIngrediente) :Observable<IReceitaIngrediente>{
+    return this.http.put<IReceitaIngrediente>(`${this.url2}/${receitaIngrediente.id}`, receitaIngrediente);
+  }
   excluirReceita(id: number): Observable<IReceitas> {
     return this.http.delete<any>(`${this.url}/${id}`);
+  }
+  excluirReceitaIngrediente(id: number): Observable<IReceitas> {
+    return this.http.delete<any>(`${this.url2}/${id}`);
   }
 }
